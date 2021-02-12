612a83d56b6e7f9124a09d119f08f8db7b6e954c
Orlando <orlando.junior07@hotmail.com>
2021-02-12 16:59:06 -0300

    Os usos de Arrow Functions

---
+++ cypress/integration/arrow.spec.js
@@ -0,0 +1,33 @@
+const { it } = require("mocha");
+
+it('nada agora', function() { })
+
+// function soma(a, b){
+//     return a + b;
+// }
+
+// const soma = function (a , b){
+//     return a + b;
+// }
+
+// const soma = (a , b) => a + b
+
+// const soma = (a, b) => {
+//     return(a+b);
+// }
+
+// const soma = (a) => a + a
+
+// const soma = a => a + a;
+
+// const soma = () => 5 + 5
+
+console.log(soma(3,4))
+
+it('a function test...', function() {
+    console.log('Function', this)
+})
+
+it('a arrow test...', () => {
+    console.log('Arrow', this)
+})
