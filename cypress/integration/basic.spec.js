e7b21fd9e7fd87d5d24ef25e1563b636189dcf0b
Orlando <orlando.junior07@hotmail.com>
2021-02-18 17:57:05 -0300

    Acessando uma página e verificando o título

---
+++ cypress/integration/basic.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('Cypress basics', () => {
+    it('Should visit a page and assert title', () =>{
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+    // const title = cy.title();
+    //console.log(title);
+
+    cy.title().should('be.equal', 'Campo de Treinamento');
+    cy.title().should('contain', 'Campo');
+
+    cy.title()
+        .should('be.equal', 'Campo de Treinamento')
+        .and('contain', 'Campo');
+
+    //TODO imprimir a log no console.
+    //TODO escrever o título no campo de texto
+    })
+    
+})
